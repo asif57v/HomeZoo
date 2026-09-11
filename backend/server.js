cd6208b6168a727b732201738a891a680821a631
Asif Mansoori <asifmansoori076@gmail.com>
2026-09-11 17:59:19 +0530

    new deploy

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,9 @@ const io = new Server(server, {
       'https://homezoo.vercel.app',
       'homezoo.vercel.app',
       'https://homezoo-rust.vercel.app',
-      'homezoo-rust.vercel.app'
+      'homezoo-rust.vercel.app',
+      'https://hoomzo.com/',
+      'https://www.hoomzo.com/'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
